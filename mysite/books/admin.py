830bb8cf8161d2897e4b43907e2589b51080c811
unknown <ettian@3CNL13669.NOE.Nokia.com>
2012-08-03 22:43:33 +0800

    add ordring

--- mysite/books/admin.py
+++ mysite/books/admin.py
@@ -8,6 +8,8 @@ class AuthorAdmin(admin.ModelAdmin):
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'publisher', 'publication_date')
     list_filter = ('publication_date',)
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)
     
 admin.site.register(Publisher)
 admin.site.register(Author, AuthorAdmin)
