b60d5391830660b4ce9dbaae2df9e3a03b8a7d8f
Xi Tian <ethan.tian@nokia.com>
2012-12-06 17:11:13 +0800

    blabla

--- bloghub/urls.py
+++ bloghub/urls.py
@@ -21,6 +21,7 @@ urlpatterns = patterns('',
     (r'^$', main_page),
     (r'^user/(\w+)/$', user_page),
     (r'^tag/([^\s]+)/$', tag_page),
+    (r'^search/$', search_page),
     # Session management
     (r'^login/$', 'django.contrib.auth.views.login'),
     (r'^logout/$', logout_page),                    
