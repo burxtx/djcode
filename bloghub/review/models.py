a8c60ab13e3db847acf5aec6e34d16efa14b40d5
TianXi <tianxi@tianxi-laptop.(none)>
2013-01-14 23:39:02 +0800

    create review model

--- bloghub/review/models.py
+++ bloghub/review/models.py
@@ -3,4 +3,33 @@ from django.contrib.auth.models import User
 
 class Review(model.Model):
 	user = models.ForeignKey(User)
+	user_name = models.CharField(max_length=50)
+	user_email = models.EmailField()
+
 	score = models.FloatField(choices=SCORE_RANGE, default=3.0)
+	comment = model.TextField(max_length=255)
+	pub_date = models.DateTimeField(auto_now_add=True)
+	
+	class Meta:
+        ordering = ("-pub_date", )
+
+    def __unicode__(self):
+        return "%s (%s)" % (self.name, self.score)
+
+	@property
+    def name(self):
+        """Returns the stored user name.
+        """
+        if self.user is not None:
+            return self.user.get_full_name()
+        else:
+            return self.user_name
+
+    @property
+    def email(self):
+        """Returns the stored user email.
+        """
+        if self.user is not None:
+            return self.user.email
+        else:
+            return self.user_email
