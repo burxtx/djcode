7145c4189ca2856436928133fba02242c7f52893
Xi Tian <ethan.tian@nokia.com>
2013-01-15 17:42:44 +0800

    create review view

--- bloghub/blog/views.py
+++ bloghub/blog/views.py
@@ -1,4 +1,3 @@
-#coding: UTF-8
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.models import User
--- bloghub/review/views.py
+++ bloghub/review/views.py
@@ -1,1 +1,34 @@
-# Create your views here.
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.forms import ModelForm, get_object_or_404
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.contrib.auth.models import User
+
+from reviews.models import Review
+
+class ReviewForm(object):
+	"""docstring for ReviewForm"""
+	class Meta:
+		model = Review
+		# fields = ('user_name', 'user_email', 'comment', 'score')
+		fields = ('comment', 'score')
+
+def ReviewForm(request):
+	if request.method == "POST":
+    form = ReviewForm(request.POST)
+        if form.is_valid():
+        	review = Review.objects.create(
+        		user=request.user)
+            review.score = form.cleaned_data['score']
+            review.comment = form.cleaned_data['comment']
+            review.save()
+            return HttpResponseRedirect(
+                '/blogpost/%s/' % 
+            )
+
+    else:
+    	form = ReviewForm
+    variables = RequestContext(request, {
+        'form':form
+        })
+    return render_to_response('review_save.html', variables)
